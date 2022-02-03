f91c0a2ead7b983f71ab7d963b2c8a11424ca217
KAMBAYASHI Yuya <kambayashi73@gmail.com>
2022-02-03 23:29:16 +0900

    add test

--- src/__test__/App.test.tsx
+++ src/__test__/App.test.tsx
@@ -6,9 +6,14 @@ import App from "../App";
 test("App works", () => {
   const { container } = render(<App />);
 
+  console.log("container");
   console.log(container);
-  const buttons = container.querySelectorAll("button");
 
-  console.log(buttons);
+  const buttons = container.querySelectorAll("button");
   expect(buttons[0].textContent).toBe("Start");
+
+  const divs = container.querySelectorAll("div");
+  console.log("divs[0]");
+  console.log(divs[1]);
+  expect(divs[1].textContent).toBe("Welcome To WordleJa");
 });
