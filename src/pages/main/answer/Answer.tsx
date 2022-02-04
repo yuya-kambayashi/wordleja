4f5d0a2a8fcff592ee83a31c7984fdf824ca9358
KAMBAYASHI Yuya <kambayashi73@gmail.com>
2022-02-04 18:12:08 +0900

    回答のレイアウトを調整します

--- src/pages/main/answer/Answer.tsx
+++ src/pages/main/answer/Answer.tsx
@@ -1,35 +1,37 @@
 import React from "react";
 import AnswerLetter from "./AnswerLetter";
 import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
+import { Stack } from "@mui/material";
 import clsx from "clsx";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
+    answer: {
+      alignItems: "flex-start"
+    },
     answerLetterUnmatch: {
       backgroundColor: "#787C7E"
     },
     answerLetter: {
       color: "#FFFFFF",
       width: "62px",
-      height: "62px",
+      height: "62x",
       fontSize: "2rem",
       fontWeight: "bold"
-      // justifyContent: "center"
+    },
+    answerLetter1: {
+      color: "#FFFFFF",
+      width: "62px",
+      height: "62x",
+      fontSize: "2rem",
+      fontWeight: "bold",
+      marginTop: "0px"
     },
     answerLetterPartialMatch: {
       backgroundColor: "#C9B458"
     },
     answerLetterExactMatch: {
       backgroundColor: "#6AAA64"
-    },
-    answer: {
-      display: "block"
-      // marginLeft: "auto",
-      // marginRight: "auto"
-      // top: "100%",
-      // left: "50%",
-      // right: "50%",
-      // marginRight: "-50%"
     }
   })
 );
@@ -42,10 +44,11 @@ const Answer: React.FC<Props> = ({ answer }) => {
 
   return (
     <>
-      <div className={classes.answer}>
+      <div></div>
+      <Stack direction="row" spacing={1} className={classes.answer}>
         <AnswerLetter
           answerLetter={answer.substring(0, 1)}
-          className={clsx(classes.answerLetter, classes.answerLetterUnmatch)}
+          className={clsx(classes.answerLetter1, classes.answerLetterUnmatch)}
         />
         <AnswerLetter
           answerLetter={answer.substring(1, 2)}
@@ -66,7 +69,7 @@ const Answer: React.FC<Props> = ({ answer }) => {
           answerLetter={answer.substring(4, 5)}
           className={clsx(classes.answerLetter, classes.answerLetterUnmatch)}
         />
-      </div>
+      </Stack>
     </>
   );
 };
--- src/pages/main/answer/Answers.tsx
+++ src/pages/main/answer/Answers.tsx
@@ -1,20 +1,35 @@
 import React from "react";
 import Answer from "./Answer";
-
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
+import { Stack } from "@mui/material";
+import clsx from "clsx";
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    answers: {
+      position: "absolute",
+      top: "50%",
+      left: "50%",
+      transform: "translate(-50%, -50%)",
+      alignItems: "flex-start"
+    }
+  })
+);
 type Props = {
   answers: string;
 };
 
 const Answers: React.FC<Props> = ({ answers }) => {
+  const classes = useStyles();
+
   return (
     <>
-      <div>{"ANSWER:"}</div>
-      <div>{"========"}</div>
-      <div>{<Answer answer={answers.substring(0, 5)} />}</div>
-      <div>{answers.substring(5, 10)}</div>
-      <div>{answers.substring(10, 15)}</div>
-      <div>{answers.substring(15, 20)}</div>
-      <div>{"========"}</div>
+      <Stack spacing={-1.5} className={classes.answers}>
+        <Answer answer={answers.substring(0, 5)} />
+        <Answer answer={answers.substring(5, 10)} />
+        <Answer answer={answers.substring(10, 15)} />
+        <Answer answer={answers.substring(15, 20)} />
+        <Answer answer={answers.substring(20, 25)} />
+      </Stack>
     </>
   );
 };
