c4030d84fb76981dab806de7669a5c627c63b0c0
KAMBAYASHI Yuya <kambayashi73@gmail.com>
2022-02-07 09:59:58 +0900

    Closes #10

--- src/pages/main/header/Header.tsx
+++ src/pages/main/header/Header.tsx
@@ -1,12 +1,45 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
+import { Stack, Typography, Divider } from "@mui/material";
+import { HelpOutlineOutlined, BarChart, Settings } from "@mui/icons-material";
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    Title: {},
+    Divider: { marginLeft: "30%", marginRight: "30%", color: "#787C7E" },
+    Icon: { fontSize: "3em", color: "#787C7E" }
+  })
+);
+type Props = {};
 const Header: React.FC<Props> = () => {
-  const navigation = useNavigate();
+  const classes = useStyles();
 
   return (
     <>
-      <div>{"Wordle"}</div>
+      <Stack
+        direction="row"
+        spacing={20}
+        style={{ justifyContent: "center", alignItems: "center" }}
+      >
+        <HelpOutlineOutlined style={{ fontSize: "2em", color: "#787C7E" }} />
+        <Typography
+          className={classes.Title}
+          style={{ fontSize: "3em", fontWeight: "bold" }}
+        >
+          {"WORDLE"}
+        </Typography>
+        <Stack direction="row">
+          <BarChart
+            className={classes.Icon}
+            style={{ fontSize: "2em", color: "#787C7E" }}
+          />
+          <Settings style={{ fontSize: "2em", color: "#787C7E" }} />
+        </Stack>
+      </Stack>
+      <Divider
+        className={classes.Divider}
+        style={{ marginLeft: "30%", marginRight: "30%" }}
+      />
     </>
   );
 };
