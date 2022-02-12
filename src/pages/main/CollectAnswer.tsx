52593d62244a60adc74e56ae6e2f0044019b4fe8
yuya-kambayashi <yuya-kambayashi@kke.co.jp>
2022-02-12 23:25:57 +0900

    #6 固定の正答の配列からランダムで正答を決めます

--- src/pages/main/CollectAnswer.tsx
+++ src/pages/main/CollectAnswer.tsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, {useEffect} from "react";
 
 type Props = {
   onSetCollectAnswer: (collectAnswer: string) => void;
 };
 const CollectAnswer: React.FC<Props> = ({ onSetCollectAnswer }) => {
-  onSetCollectAnswer("YIELD");
+
+  const candidates = ["ABOUT", "YIELD", "STORY"];
+
+  const getRandomIndex = (min : number, max : number) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+  useEffect(() => {
+
+
+    onSetCollectAnswer(candidates[getRandomIndex(0, candidates.length)]);
+  }, [])
 
   return <></>;
 };
